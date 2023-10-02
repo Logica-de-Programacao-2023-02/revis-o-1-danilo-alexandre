ff94323ddee438f04547490f8ddbeaed056930c1
Danilomiziara <vazdanilo08@gmail.com>
2023-10-01 21:40:06 -0300

    resolvido

--- q4/q4.go
+++ q4/q4.go
@@ -1,5 +1,7 @@
 package q4
 
+import "fmt"
+
 //Uma loja virtual de roupas recebeu várias listas de produtos vendidos em diferentes dias da semana. O dono da loja
 //deseja analisar as listas para entender melhor o comportamento de suas vendas. Para isso, ele precisa classificar cada
 //lista como em ordem crescente, decrescente ou aleatória, de acordo com o preço dos produtos.
@@ -10,5 +12,29 @@ package q4
 //Caso a lista possua apenas um elemento, a função deve retornar 3.
 
 func ClassifyPrices(prices []int) (int, error) {
-	return 0, nil
+	if len(prices) == 0 {
+		return 0, fmt.Errorf("Erro: lista vazia")
+	}
+	if len(prices) == 1 {
+		return 3, nil
+	}
+	crescente := true
+	decrescente := true
+	for i := 1; i < len(prices); i++ {
+		if prices[i-1] < prices[i] {
+			decrescente = false
+		}
+		if prices[i-1] > prices[i] {
+			crescente = false
+		}
+
+	}
+	if crescente {
+		return 1, nil
+	} else if decrescente {
+		return 2, nil
+	} else {
+		return 3, nil
+	}
+
 }
--- q5/q5.go
+++ q5/q5.go
@@ -1,5 +1,9 @@
 package q5
 
+import (
+	"strings"
+)
+
 //Pedro começou a frequentar aulas de programação. Na primeira aula, sua tarefa foi escrever um programa simples. O
 //programa deveria fazer o seguinte: na sequência de caracteres fornecida, composta por letras latinas maiúsculas e
 //minúsculas, ele:
@@ -15,5 +19,23 @@ package q5
 //Ajude Pedro a lidar com esta tarefa fácil.
 
 func ProcessString(s string) string {
-	return ""
+	// Inicializa uma string vazia para armazenar o resultado.
+	resultado := ""
+
+	// Define uma string contendo todas as vogais em minúsculas.
+	vogais := "aeiouAEIOU"
+
+	// Itera pelos caracteres da string fornecida.
+	for _, char := range s {
+		charStr := string(char) // Converte o caractere para string.
+
+		// Verifica se o caractere não é uma vogal.
+		if !strings.ContainsAny(charStr, vogais) {
+			// Se não for uma vogal, adiciona um "." antes do caractere.
+			resultado += "." + charStr
+			resultado = strings.ToLower(resultado)
+		}
+	}
+
+	return resultado
 }
